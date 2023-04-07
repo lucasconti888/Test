f86cdcd134c5401e3446aabbf9dcf6f1652b8ee7
Lucas Conti <lucas.pereira@sou.inteli.edu.br>
2023-04-07 16:12:35 -0300

    Slide + menu

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,35 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Facebook from './components/Media';
-import { Grid, Box, Typography, Button } from '@mui/material';
+import { Grid, Box, Typography, Button, Slider } from '@mui/material';
 
-const DashboardPage = () => {
-    // function handleClick() {
-    //     const audio = new Audio(somBotao);
-    //     audio.play();
-    //   }
+const DashboardPage = (props) => {
+    const [value, setValue] = useState(0)
+    const [loading, setLoading] = useState(props.loading);
+
+    function handleSlide(event) {
+        setValue(event.target.value);
+    }
     const location = useLocation();
     const account = location.state ? location.state.account : '';
 
     return (
-        <Box sx={{ backgroundColor: 'rgba(126, 149, 255, 0.8)', padding: 3 }}>
+        <Box sx={{
+            // backgroundColor: 'rgba(126, 149, 255, 0.6)', 
+            // background: 'linear-gradient(to bottom, #33ccff 0%, #ff99cc 100%)',
+            background: 'radial-gradient(circle, rgba(82,49,128,0.8) 47%, rgba(82,49,128,0.8) 79%, rgba(82,49,128,0.8) 90%)',
+        
+        
+        padding: 3, minHeight:'90vh'
+        }}>
 
-            <Grid sx={{padding:2}}>
-                <Typography sx={{fontWeight:800, }}>Welcome to the Dashboard, {account}!</Typography>
-                {account && <Typography sx={{fontWeight:600, }}>You are logged in as {account}.</Typography>}
+            <Grid sx={{ padding: 2 }}>
+                <Typography sx={{ fontWeight: 800, }}>Welcome to the Dashboard, {account}!</Typography>
+                {account && <Typography sx={{ fontWeight: 600, }}>You are logged in as {account}.</Typography>}
                 <Typography>This is your dashboard page.</Typography>
-                {/* <Button onClick={handleClick}>Clique Aqui</Button> */}
+                <Slider
+                    size="small"
+                    defaultValue={0}
+                    aria-label="Small"
+                    valueLabelDisplay="auto"
+                    value={value}
+                    onChange={handleSlide}
+                    max={750}
+                />
             </Grid>
 
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                <Grid item xs={6}>
-                    <Facebook />
-                </Grid>
-                <Grid item xs={6}>
-                    <Facebook />
-                </Grid>
-            </Grid>
+            <Box sx={{
+                marginLeft: loading ? 1 : `${value}px`,
+                transition: 'margin-left 0.5s ease-out'
+            }}>
+                <Facebook />
+            </Box>
+
+            {value >= 300 && (
+                <>
+                    <Grid
+                        sx={{
+                            position: "fixed",
+                            top: "30vh",
+                            left: "10vh",
+                            opacity: Math.min((value - 300) / 150, 1),
+                            transition: "opacity 2s ease-in",
+                            display: "flex",
+                            flexDirection: "column",
+                        }}
+                    >
+                        <Button
+                            variant="contained"
+                            sx={{
+                                width: "70vh",
+                                height: "8vh",
+                                justifyContent: "flex-start",
+                                paddingLeft: 4,
+                                textTransform: "none",
+                                fontSize: "18px",
+                                backgroundColor: "rgba(126, 149, 255, 1)",
+                            }}
+                        >
+                            Ver dados
+                        </Button>
+                        <Button variant='contained' sx={{
+                            width: '70vh', height: '8vh', justifyContent: 'flex-start', paddingLeft: 4, textTransform: 'none', fontSize: '18px', marginTop: 3,
+                            backgroundColor: 'rgba(126, 149, 255, 1)'
+                        }}>
+                            Ver dados
+                        </Button>
+                    </Grid>
+                </>
+            )}
+
         </Box>
+
     );
 };
 
--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
@@ -7,85 +7,104 @@ import CardMedia from '@mui/material/CardMedia';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-// import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Skeleton from '@mui/material/Skeleton';
 import somBotao from '../audio/stout.mp3';
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import { Button, Grid, Icon } from '@mui/material';
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
 
 function Media(props) {
-  const { loading = false } = props;
-function handleClick() {
-        const audio = new Audio(somBotao);
-        audio.play();
-      }
+  const [loading, setLoading] = useState(props.loading);
+  const audio = new Audio(somBotao);
+
+  function handleClick() {
+    audio.play();
+    setLoading(true);
+  }
+
+  function handleClick2() {
+    setLoading(false);
+  }
+
+  function handlePause() {
+    audio.pause();
+    audio.currentTime = 0;
+    console.log('pause')
+  }
+
 
   return (
-    <Card sx={{ maxWidth: 345, m: 2 }}>
-      <CardHeader
-        avatar={
-          loading ? (
-            <Skeleton animation="wave" variant="circular" width={40} height={40} />
-          ) : (
-            <Avatar
-              alt="Ted talk"
-              src="https://pbs.twimg.com/profile_images/877631054525472768/Xp5FAPD5_reasonably_small.jpg"
-            />
-          )
-        }
-        action={
-          loading ? null : (
-            <IconButton aria-label="settings" onClick={handleClick}>
-              <MusicNoteIcon />
-            </IconButton>
-          )
-        }
-        title={
-          loading ? (
-            <Skeleton
-              animation="wave"
-              height={10}
-              width="80%"
-              style={{ marginBottom: 6 }}
-            />
-          ) : (
-            'Ted'
-          )
-        }
-        subheader={
-          loading ? (
-            <Skeleton animation="wave" height={10} width="40%" />
-          ) : (
-            '5 hours ago'
-          )
-        }
-      />
-      {loading ? (
-        <Skeleton sx={{ height: 190 }} animation="wave" variant="rectangular" />
-      ) : (
-        <CardMedia
-          component="img"
-          height="140"
-          image="https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/72bda89f-9bbf-4685-910a-2f151c4f3a8a/NicolaSturgeon_2019T-embed.jpg?w=512"
-          alt="Nicola Sturgeon on a TED talk stage"
+    <>
+      <Card sx={{ maxWidth: 345, m: 2, opacity: loading ? 0 : 1,
+        transition: 'opacity 5s ease-out'
+ }}>
+        <CardHeader
+          avatar={
+            loading ? (
+              <Skeleton animation="wave" variant="circular" width={40} height={40} />
+            ) : (
+              <Avatar
+                alt="Ted talk"
+                src="https://pbs.twimg.com/profile_images/877631054525472768/Xp5FAPD5_reasonably_small.jpg"
+              />
+            )
+          }
+          action={
+            loading ? null : (
+              <IconButton aria-label="settings" onClick={handleClick}>
+                <MusicNoteIcon />
+              </IconButton>
+            )
+          }
+          title={
+            loading ? (
+              <Skeleton
+                animation="wave"
+                height={10}
+                width="80%"
+                style={{ marginBottom: 6 }}
+              />
+            ) : (
+              'Ted'
+            )
+          }
+          subheader={
+            loading ? (
+              <Skeleton animation="wave" height={10} width="40%" />
+            ) : (
+              '5 hours ago'
+            )
+          }
         />
-      )}
 
-      <CardContent>
         {loading ? (
-          <React.Fragment>
-            <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
-            <Skeleton animation="wave" height={10} width="80%" />
-          </React.Fragment>
+          <Skeleton sx={{ height: 190 }} animation="wave" variant="rectangular" />
         ) : (
-          <Typography variant="body2" color="text.secondary" component="p">
-            {
-                
-              "Why First Minister of Scotland Nicola Sturgeon thinks GDP is the wrong measure of a country's success:"
-            }
-          </Typography>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/72bda89f-9bbf-4685-910a-2f151c4f3a8a/NicolaSturgeon_2019T-embed.jpg?w=512"
+            alt="Nicola Sturgeon on a TED talk stage"
+          />
         )}
-      </CardContent>
-    </Card>
+
+        <CardContent>
+          {loading ? (
+            <React.Fragment>
+              <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
+              <Skeleton animation="wave" height={10} width="80%" />
+            </React.Fragment>
+          ) : (
+            <Typography variant="body2" color="text.secondary" component="p">
+              {
+                "Why First Minister of Scotland Nicola Sturgeon thinks GDP is the wrong measure of a country's success:"
+              }
+            </Typography>
+          )}
+        </CardContent>
+      </Card>
+    </>
   );
 }
 
@@ -93,11 +112,45 @@ Media.propTypes = {
   loading: PropTypes.bool,
 };
 
+
 export default function Facebook() {
-  return (
-    <div>
-      <Media loading />
-      <Media />
-    </div>
+  const [numCards, setNumCards] = useState(1);
+  const cards = [
+    { id: 1, loading: false },
+    { id: 2, loading: false },
+    { id: 3, loading: false },
+    { id: 4, loading: false },
+    { id: 5, loading: false },
+    { id: 6, loading: false },
+    // add more cards as needed
+  ];
+  
+  function handleButtonClick() {
+    setNumCards(numCards + 1);
+  }
+    
+
+  function handleButtonClick2() {
+    setNumCards(numCards - 1);
+  }
+
+
+  return (<>
+    <IconButton onClick={handleButtonClick}><AddIcon/></IconButton>
+    <IconButton onClick={handleButtonClick2}><RemoveIcon/></IconButton>
+    <Grid container spacing={2} sx={{ mt: 4 }}>
+    {cards.slice(0, numCards).map((card) => (
+          <Grid item xs={12} sm={6} md={4} key={card.id}>
+            <Media loading={card.loading} />
+          </Grid>
+        ))}
+       
+      {/* {cards.map((card) => (
+        <Grid item xs={12} sm={6} md={4} key={card.id}>
+          <Media loading={card.loading} />
+        </Grid>
+      ))} */}
+    </Grid>
+    </>
   );
 }
