32ab361a59b381ff7783fad1fb9d15e404cd8d99
Lucas Conti <lucas.pereira@sou.inteli.edu.br>
2023-04-14 17:46:03 -0300

    hihihih

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Grid, Box, Typography, Button, Slider, Paper } from '@mui/material';
+import { Grid, Box, Typography, Button, Slider, Paper, Divider } from '@mui/material';
 import Switch from '@mui/material/Switch';
 const label = { inputProps: { 'aria-label': 'Switch demo' } };
 import * as THREE from 'three';
@@ -36,7 +36,7 @@ const DashboardPage = () => {
     // ........
 
     const paperStyle1 = {
-        backgroundColor: 'white', height: '65vh', width: '100vh', display: 'flex', justifyContent: 'center', paddingTop: 2,
+        backgroundColor: 'transparent', height: '65vh', width: '100vh', display: 'flex', justifyContent: 'center', paddingTop: 2,
         opacity: 0, // Set initial opacity to 0
         transition: 'opacity 0.5s ease-in', 
         boxShadow: 0,
@@ -45,11 +45,11 @@ const DashboardPage = () => {
 
     const paperStyle2 = {
         backgroundColor: '#1e1e5d',
-        height: '15vh',
-        width: '20vh',
-        '@media (max-width: 400px)': {
-            width:'15vh', height:'10vh', 
-          },
+        height: '10vh',
+        width: '15vh',
+        // '@media (max-width: 400px)': {
+        //     width:'20vh', height:'15vh', 
+        //   },
         display: 'flex',
       borderRadius:40,
         justifyContent: 'center',
@@ -67,7 +67,7 @@ const DashboardPage = () => {
 
     }
     const paperStyle3_2 = {
-        backgroundColor: 'white', display: 'flex', justifyContent: 'center', paddingTop: 5, boxShadow: 0,
+        backgroundColor: 'transparent', display: 'flex', justifyContent: 'center', paddingTop: 5, boxShadow: 0,
         opacity: 0, // Set initial opacity to 0
         transition: 'opacity 0.5s ease-in', marginBottom: 5,
 
@@ -135,17 +135,20 @@ const DashboardPage = () => {
 
     return (
         <>
+        
             <Typography ref={componentRefText_1} sx={{ marginTop: 10, fontSize: 26, color:'#494d5f',opacity:0, transition: 'margin-left 0.5s ease-in, opacity 0.5s ease-in', }}>Olá, eu sou o Lucas</Typography>
             <Typography ref={componentRefText_2} sx={{ marginBottom: 2,  fontSize: 20, color: '#587ffc', opacity:0, transition: 'margin-left 0.5s ease-in, opacity 0.5s ease-in', }}>A gabi ama o lu</Typography>
+            <Divider variant={'middle'}/>
             <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '5vh' }}>
                 <Paper ref={componentRef1} sx={paperStyle1}>
                     <img src={gifComp} alt="GIF" />
                 </Paper>
-
             </Box>
-
+            <Divider  sx={{marginTop:5}}/>
             <Typography ref={componentRefText_3} sx={{ marginTop: 8, marginLeft: '20vh',  color:'#494d5f',fontSize: 26, opacity:0, transition: 'margin-left 0.5s ease-in, opacity 0.5s ease-in', }}>Teste para texto com three.js</Typography>
             <Typography ref={componentRefText_4} sx={{ marginBottom: 2, marginLeft: '20vh', fontSize: 20, color: '#587ffc', opacity:0, transition: 'margin-left 0.5s ease-in, opacity 0.5s ease-in', }}>ele é o amor da vida dela </Typography>
+            <Divider variant={'middle'}/>
+
             <Grid columnGap={5} spacing={2} rowGap={15} container sx={{ display: 'flex', justifyContent: 'center', marginTop: 10, }} >
             <Grid item ><Paper ref={componentRef2_1} sx={paperStyle2}>SIMMMM</Paper></Grid>
             <Grid item>   <Paper ref={componentRef2_2} sx={paperStyle2}></Paper></Grid>
@@ -159,9 +162,12 @@ const DashboardPage = () => {
             <Grid item > <Paper ref={componentRef3_5} sx={paperStyle2}> </Paper></Grid>
             </Grid>
 
+            <Divider sx={{ marginTop:10, }}/>
 
             <Typography ref={componentRefText_5} sx={{ marginTop: 10, marginLeft: '20vh' , color:'#494d5f', fontSize: 20,  opacity:0, transition: 'margin-left 0.5s ease-in, opacity 0.5s ease-in', }}>Entre em</Typography>
             <Typography ref={componentRefText_6} sx={{ marginLeft: '20vh', marginBottom: 6, fontSize: 26 ,color: '#587ffc',opacity:0, transition: 'margin-left 0.5s ease-in, opacity 0.5s ease-in',}}>Contato</Typography>
+            <Divider variant={'middle'}/>
+
 
             <Box sx={{ display: 'flex', justifyContent: 'space-around', marginTop: 4, marginRight: 5 }}>
             <Grid columnGap={5} spacing={2} rowGap={15} container sx={{ }} >
